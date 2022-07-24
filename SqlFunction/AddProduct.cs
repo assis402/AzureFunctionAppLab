188c0e4cec83c048f4e9d8cf118a8cc7f88c7a02
Matheus de Assis Alcântara <matheus.labsit@dotzmkt.com.br>
2022-07-24 11:36:03 -0300

    addproduct

---
+++ SqlFunction/AddProduct.cs
@@ -0,0 +1,45 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
+
+namespace SqlFunction
+{
+    public static class AddProduct
+    {
+        [FunctionName("AddProduct")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var data = JsonConvert.DeserializeObject<Product>(requestBody);
+
+            var statement = $"INSERT INTO Products(ProductID, ProductName, Quantity) VALUES ({data.ProductId}, '{data.ProductName}', {data.Quantity})";
+
+            var connection = GetConnection();
+            connection.Open();
+
+            using (var command = new SqlCommand(statement, connection))
+            {
+                command.CommandType = System.Data.CommandType.Text;
+                command.ExecuteNonQuery();
+            }
+
+            connection.Close();
+            return new OkObjectResult("Product added");
+        }
+
+        private static SqlConnection GetConnection()
+        {
+            var connectionString = "{sqldbconnectionstring}";
+            return new SqlConnection(connectionString);
+        }
+    }
+}
